350d8570e393bdbd7c888547a6d8168a230cba39
mayurkodavala1810-Design <mayurkodavala1810@gmail.com>
2026-07-13 18:32:10 +0000

    This Is Complate to pattern

--- Pattern_Solve/05_Pattern.py
+++ Pattern_Solve/05_Pattern.py
@@ -1,8 +1,19 @@
-# for i in range(5,0,-1):
-#     for j in range(1,i + 1):
-#         print(i, end=" ")
-#     print()
+# 5 5 5 5 5
+# 4 4 4 4
+# 3 3 3
+# 2 2
+# 1
+for i in range(5,0,-1):
+    for j in range(1,i + 1):
+        print(i, end=" ")
+    print()
 
+
+# 1 2 3 4 5
+# 1 2 3 4
+# 1 2 3
+# 1 2
+# 1
 for i in range(5,0,-1):
     for j in range(1,i + 1):
         print(j, end=" ")
--- Pattern_Solve/06_pattren.py
+++ Pattern_Solve/06_pattren.py
@@ -1,3 +1,9 @@
+# 5 4 3 2 1 
+# 5 4 3 2 
+# 5 4 3 
+# 5 4 
+# 5 
+
 n = int(input("Enter a number:- "))
 
 for i in range(1,n+1):
