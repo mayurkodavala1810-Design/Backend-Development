54287bb91b93c144bf5e5c8a53423704ccc83146
MayurKodavala <mayurkodavala1810@gmail.com>
2026-07-22 14:32:57 +0000

    This is Basic List Methods

---
+++ Chepter 6/List in Python/03_Indexing.py
@@ -0,0 +1,12 @@
+lst = ["Anirudh", "Sanjay", 56, 91.8120, 100, "Surat"]
+
+
+# print(lst[0])
+# print(lst[5])
+# print(lst[-1])
+# print(lst[-5])
+# print(lst[2])
+print(lst[0:5:2])
+
+n = len(lst)
+print(f"last element = {lst[n-1]}")
---
+++ Chepter 6/List in Python/04_Updating.py
@@ -0,0 +1,7 @@
+lst = [1,5,8,1,3,4,"Mayur","Anuj"]
+print(f"Before List of {lst}")
+
+lst[1] = 3
+lst[5] = "Raj"
+lst[2] = True
+print(f"Now List of {lst}")
