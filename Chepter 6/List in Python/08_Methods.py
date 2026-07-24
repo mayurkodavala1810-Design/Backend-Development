f4dba6a59b87cd0af1cfed31d967f712c37414ea
MayurKodavala <mayurkodavala1810@gmail.com>
2026-07-24 18:58:24 +0000

    This is Advance List Learing

---
+++ Chepter 6/List in Python/08_Methods.py
@@ -0,0 +1,13 @@
+lst = ["Anirudh", 54, 21.98, True, "Surat", 54, 54]
+# print(lst)
+# print(lst, id(lst))
+# lst.append(100)
+# lst.append("Delhi")
+# lst.insert(2, "Delhi")
+# lst.insert(0, "Muskan")
+
+# lst.pop()
+# lst.pop(0)  # By index
+
+# lst.remove("Anirudh")
+print(lst, id(lst))
---
+++ Chepter 6/List in Python/11_MemberShip_Operator.py
@@ -0,0 +1,13 @@
+nums = [4, 7, 3, 8, 1, 1, 2, 10, 9, 6, 9, 1, 1, 1]
+
+target = int(input("Enter target = "))
+
+# while target in nums:
+#     nums.remove(target)
+# print(f"Num =  {nums}")
+
+if target in nums:
+    nums.remove(target)
+    print(f"nums = {nums}")
+else:
+    print("Cannot remove target, target does not exists")
